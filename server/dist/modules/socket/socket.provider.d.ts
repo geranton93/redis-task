c0d5ec47dd47c8bfa14ab21caed1faf45ddfd1bb
geranton93 <Xl1ve93@yandex.ru>
2019-12-03 09:52:03 +0200

    clean up

--- server/dist/modules/socket/socket.provider.d.ts
+++
@@ -1,7 +0,0 @@
-import * as io from 'socket.io';
-import { ConfigService } from '../config/config.service';
-export declare const wsProvider: {
-    provide: string;
-    useFactory: (configService: ConfigService) => Promise<io.Server>;
-    inject: (typeof ConfigService)[];
-};
